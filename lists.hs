1d7523a95e549ce8760fcc3988352c830855f426
Pedro Pazello <pedropazello@gmail.com>
2015-02-09 20:51:42 -0200

    1~10 ()

--- lists.hs
+++ lists.hs
@@ -6,6 +6,7 @@ myLast (x:xs)
     | length xs == 0 = x
     | otherwise      = myLast xs
 
+
 -- Problem 2
 -- Find the last but one element of a list.
 
@@ -37,6 +38,7 @@ myReverse :: [a] -> [a]
 myReverse [] = []
 myReverse (x:xs) = myReverse xs ++ [x]
 
+
 -- Problem 6
 -- Find out whether a list is a palindrome. A palindrome can be read forward or backward; e.g. (x a m a x). 
 
@@ -52,6 +54,8 @@ isPalindrome xs
 Transform a list, possibly holding lists as elements into a `flat' 
 list by replacing each list with its elements (recursively).
 --}
+
+-- not implemented yet
 
 -- Problem 8
 {-- 
@@ -88,3 +92,6 @@ method. Consecutive duplicates of elements are encoded as
 lists (N E) where N is the number of duplicates of the element E. 
 --}
 
+encode :: Eq a => [a] -> [(Int, a)]
+encode [] = []
+encode xs = [(length x, head x) | x <- pack xs]
